988a88ad30c40cf6f17e90233e0de94cd585037e
TurretSeven <74435634+TurretSeven@users.noreply.github.com>
2022-07-26 20:18:23 +0100

    Delete exiftooltest.py
    Deleted file

--- exiftooltest.py
+++
@@ -1,8 +0,0 @@
-import exiftool
-
-files = ["/Users/steve/Temp/PhotoTest/New Layout/2022/01/22/Watloo Sunset 1.tif"]
-with exiftool.ExifToolHelper() as et:
-    metadata = et.get_metadata(files)
-
-for d in metadata:
-    print("{:20.20} {:20.20}".format(d["SourceFile"], d["EXIF:DateTimeOriginal"]))
